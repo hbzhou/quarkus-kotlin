fa08c1b2096d46776da821d6ba20c61d9e445cff
Jeremy_Zhou <Jeremy_Zhou@epam.com>
2023-07-03 11:11:19 +0800

    add api for downloading file with Vertx

---
+++ src/main/kotlin/com/itsz/quarkus/resource/VertxResource.kt
@@ -0,0 +1,26 @@
+package com.itsz.quarkus.resource
+
+import io.smallrye.mutiny.Uni
+import io.vertx.core.file.OpenOptions
+import io.vertx.mutiny.core.Vertx
+import javax.enterprise.context.ApplicationScoped
+import javax.ws.rs.GET
+import javax.ws.rs.Path
+import javax.ws.rs.Produces
+import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
+
+@ApplicationScoped
+@Path("/api/vertx")
+class VertxResource(val vertx: Vertx) {
+
+    @GET
+    @Path("/download")
+    @Produces(MediaType.APPLICATION_OCTET_STREAM)
+    fun download (): Uni<Response>{
+       return vertx.fileSystem().open("HKV04899081.mp4", OpenOptions().setRead(true))
+            .onItem().transform {
+                Response.ok(it).header("content-disposition", "attachment; filename = demo.mp4").build()
+            }
+    }
+}
