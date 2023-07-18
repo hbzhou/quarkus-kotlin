e7aedaa9506b00ff11c2f6ecc59ab73c8d616851
Jeremy Zhou <jeremygilbert@126.com>
2023-07-18 22:41:55 +0800

    tranfer file from socket and get socket id

--- src/main/kotlin/com/itsz/vertx/VertxNetClient.kt
+++ src/main/kotlin/com/itsz/vertx/VertxNetClient.kt
@@ -8,7 +8,7 @@ fun main() {
     Vertx.vertx().createNetClient(NetClientOptions().setReconnectInterval(2000).setReconnectAttempts(1000).setLogActivity(true).setActivityLogDataFormat(ByteBufFormat.SIMPLE)).connect(3000, "localhost")
         .subscribe()
         .with { socket ->
-//            socket.sendFileAndForget("book.txt")
+            socket.sendFileAndForget("book.txt")
             socket.writeAndForget("hello, from client!!\r\n")
             socket.handler {
                 println("${Thread.currentThread().name} -->receiving message from server $it")
